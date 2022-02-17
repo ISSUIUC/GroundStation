25e5a193be1e39f8f375a5a16a9f3d3561e6a087
Nicholas Phillips <nwp2@illinois.edu>
2022-02-17 17:03:36 -0600

    menu fix

--- GUI/src/index.ts
+++ GUI/src/index.ts
@@ -1,15 +1,15 @@
-import { app, BrowserWindow, Menu } from 'electron';
+import { app, BrowserWindow, Menu, MenuItemConstructorOptions } from 'electron';
 import * as SerialPort from "serialport"
 import * as path from 'path';
 
 // Initializes windows as main windows.
-let mainWinow : BrowserWindow;
+let mainWindow : BrowserWindow;
 let serialWindow : BrowserWindow;
 
 
 app.on('ready', () => {
     console.log('App is ready');
-    mainWinow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 600,
         height: 400,
         webPreferences: {
@@ -20,18 +20,20 @@ app.on('ready', () => {
     }); 
 
     const indexHTML = path.join(__dirname + '/index.html');
-    mainWinow.loadFile(indexHTML).then(() => {
-        serial_communicate(mainWinow);
+    mainWindow.loadFile(indexHTML).then(() => {
+        serial_communicate(mainWindow);
     }).catch(e => console.error(e));
-
     // Builds menu template and renders it in the main window
-    const mainMenu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(mainMenu);
+    const mainMenu = Menu.buildFromTemplate(mainWindowTemplate(mainWindow));
+    mainWindow.setMenu(mainMenu);
 });
 
 
 function serial_communicate(window: BrowserWindow){
-    setInterval(async ()=>{
+    const interval = setInterval(async ()=>{
+        if(window.isDestroyed()){
+            clearInterval(interval);
+        }
         const ports = await SerialPort.list();
         window.webContents.send("serial", JSON.stringify(ports));
     }, 1000);
@@ -53,43 +55,67 @@ function createSerialWindow() {
         }
     });
     serialWindow.loadURL(`file://${__dirname}/serial.html`);
+    const menu = Menu.buildFromTemplate(serialWindowTemplate(serialWindow));
+    serialWindow.setMenu(menu);
     serial_communicate(serialWindow);
 }
 
+function serialWindowTemplate(window: BrowserWindow) : MenuItemConstructorOptions[] {
+    return [
+        // "File" subgroup
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Quit',
+                    accelerator: 'ctrl+Q',
+                    click() { app.quit(); }
+                },
+                {
+                    label: 'Debug',
+                    accelerator: 'F12',
+                    click() { window.webContents.openDevTools() }
+                }
+            ]
+        },
+    ];
+}
+
+function mainWindowTemplate(window: BrowserWindow) : MenuItemConstructorOptions[] {
+    return [
+        // "File" subgroup
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Quit',
+                    accelerator: 'ctrl+Q',
+                    click() { app.quit(); }
+                },
+                {
+                    label: 'Debug',
+                    accelerator: 'F12',
+                    click() { window.webContents.openDevTools() }
+                }
+            ]
+        },
+    
+        // "Connct" subgroup
+        {
+           label: 'Connect',
+           submenu: [
+               {
+                   label: 'Serial',
+                   click() { createSerialWindow(); }
+               },
+               {
+                   label: 'Ethernet'
+               }
+           ] 
+        }
+    ];
+}
 // Template for menu
-const menuTemplate = [
-
-    // "File" subgroup
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                accelerator: 'ctrl+Q',
-                click() { app.quit(); }
-            },
-            {
-                label: 'Debug',
-                accelerator: 'F12',
-                click() { mainWinow.webContents.openDevTools() }
-            }
-        ]
-    },
-
-    // "Connct" subgroup
-    {
-       label: 'Connect',
-       submenu: [
-           {
-               label: 'Serial',
-               click() { createSerialWindow(); }
-           },
-           {
-               label: 'Ethernet'
-           }
-       ] 
-    }
-];
 
 
 
