316ea28ada5ba3f70d727561399e957cc7cf2dc3
Aryaman Dwivedi <dwivedi6@illinois.edu>
2022-03-21 18:37:59 -0500

    Made CSV.ts file cleaner

--- GUI/src/csv.ts
+++ GUI/src/csv.ts
@@ -9,21 +9,7 @@ const CSV_HEADERS = ["Time", "LSM_IMU_mx", "LSM_IMU_my", "LSM_IMU_mz",
     "H3L_IMU_ax", "H3L_IMU_ay", "H3L_IMU_az",
     "barometer_alt", "sign", "signal"]
 
-let date = new Date();
-let year = date.getFullYear();
-let month = date.getMonth() + 1;
-let day = date.getDate();
-let hh = date.getHours();
-let mm = date.getMinutes();
-let ss = date.getSeconds();
-let ms = date.getMilliseconds();
 
-let session = hh <= 12 ? "AM" : "PM";
-let hour = (hh < 10) ? "0" + hh : hh;
-let minute = (mm < 10) ? "0" + mm : mm;
-let second = (ss < 10) ? "0" + ss : ss;
-
-let time = year + "-" + month + "-" + day + "--" + hour + "-" + minute + "-" + second + "-" + ms;
 
 export default class CSVWriter {
     file_name: string;
@@ -35,7 +21,7 @@ export default class CSVWriter {
     write_data(data: SerialResponse) {
         if (data.type == "data") {
             const m = data.value;
-            refresh_time();
+            let time = refresh_time();
             const str = `${time},${CSV_HEADERS.slice(1).map(c => (m as any)[c]).join(',')}\n`;
             fs.appendFileSync(this.file_name, str);
         }
@@ -43,19 +29,20 @@ export default class CSVWriter {
 }
 
 function refresh_time() {
-    date = new Date();
-    year = date.getFullYear();
-    month = date.getMonth() + 1;
-    day = date.getDate();
-    hh = date.getHours();
-    mm = date.getMinutes();
-    ss = date.getSeconds();
-    ms = date.getMilliseconds();
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let ms = date.getMilliseconds();
 
-    session = hh <= 12 ? "AM" : "PM";
-    hour = (hh < 10) ? "0" + hh : hh;
-    minute = (mm < 10) ? "0" + mm : mm;
-    second = (ss < 10) ? "0" + ss : ss;
+    let session = hh <= 12 ? "AM" : "PM";
+    let hour = (hh < 10) ? "0" + hh : hh;
+    let minute = (mm < 10) ? "0" + mm : mm;
+    let second = (ss < 10) ? "0" + ss : ss;
 
-    time = year + "-" + month + "-" + day + "--" + hour + "-" + minute + "-" + second + "-" + ms;
+    let time = year + "-" + month + "-" + day + "--" + hour + "-" + minute + "-" + second + "-" + ms;
+    return time;
 }
