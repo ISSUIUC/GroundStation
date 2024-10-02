3f183e2c84cb3f54eeb5fc0c904732f502005bf1
Bryce Mikos <bmikos2@illinois.edu>
2024-10-01 23:54:25 -0500

    Adding Test Flask Host

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+# Counter variable
+counter = 0
+
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    global counter
+    return render_template('index.html', counter=counter)
+
+@app.route('/increment', methods=['POST'])  # Allow POST method
+def increment():
+    global counter
+    counter += 1
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use '0.0.0.0' to make the app accessible to other devices on the same network
+    app.run(host='0.0.0.0', port=5000)
