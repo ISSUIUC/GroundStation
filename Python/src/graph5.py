991a0220e1598565105404ce350610e0b43309d3
Bryce Mikos <bmikos2@illinois.edu>
2024-10-02 20:22:08 -0500

    Testing out flask and display of graphs

---
+++ Python/src/graph5.py
@@ -0,0 +1,16 @@
+# src/graph1.py
+# At the top of each graph script
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+import matplotlib.pyplot as plt
+
+
+def create_graph():
+    x = [1, 2, 3, 4]
+    y = [10, 20, 25, 30]
+    plt.plot(x, y)
+    plt.title("Graph 1")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.savefig('static/images/graph5.png')  # Save graph to static/images/
+    plt.close()  # Close the figure to free memory
