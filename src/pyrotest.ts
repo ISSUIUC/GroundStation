988882af84be854b76d86bc9dbfe4b4036c5ed35
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-12-16 22:15:13 -0600

    space newline

--- src/pyrotest.ts
+++ src/pyrotest.ts
@@ -1,27 +1,27 @@
 import { ipcMain, ipcRenderer } from 'electron';
 import { ServerConnection } from './serverConnection';
 
-let pt_button = <HTMLButtonElement>document.getElementById("PT");
+let pt_button = <HTMLButtonElement>document.getElementById("PT \n");
 pt_button.addEventListener("click", () => {
     ipcRenderer.send('raw_command', 'PT');
 });
 
-let pa_button = <HTMLButtonElement>document.getElementById("PA");
+let pa_button = <HTMLButtonElement>document.getElementById("PA \n");
 pa_button.addEventListener("click", () => {
     ipcRenderer.send('raw_command', 'PA');
 });
 
-let pb_button = <HTMLButtonElement>document.getElementById("PB");
+let pb_button = <HTMLButtonElement>document.getElementById("PB \n");
 pb_button.addEventListener("click", () => {
     ipcRenderer.send('raw_command', 'PB');
 });
 
-let pc_button = <HTMLButtonElement>document.getElementById("PC");
+let pc_button = <HTMLButtonElement>document.getElementById("PC \n");
 pc_button.addEventListener("click", () => {
     ipcRenderer.send('raw_command', 'PC');
 });
 
-let pd_button = <HTMLButtonElement>document.getElementById("PD");
+let pd_button = <HTMLButtonElement>document.getElementById("PD \n");
 pd_button.addEventListener("click", () => {
     ipcRenderer.send('raw_command', 'PD');
 });
