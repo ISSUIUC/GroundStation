357f05d97c1853ae1b64789a63af1b04bb0f5a88
Zyun Lam <zyunlam@gmail.com>
2026-04-07 20:24:08 -0500

    Wirk on stuff

--- gss-combiner/server.py
+++ gss-combiner/server.py
@@ -1,19 +1,29 @@
 from flask import Flask, current_app
 from flask_cors import CORS
-from util.relay_mqtt import RelayMqtt
-import serial
+from serial.tools.list_ports import comports
+import json
+
+def get_feather_duo_ports():
+    """
+    Gets the ports of connected Feather Duos
+
+    Returns:
+        list: list of connected Feather Duos
+    """
+    FEATHER_DUO_PID = 4097
+    return {"devices": [port.device for port in comports() if port.pid == FEATHER_DUO_PID]}
+
 
 app = Flask(__name__)
 
 CORS(app)
 
-relay_mqtt = RelayMqtt(app)
-
 @app.route('/list_ports')
 def list_ports():
-	serial.util.list_ports()
+	return get_feather_duo_ports()
 
-if __name__ == '__main__':
-    print("Starting server...")
-    relay_mqtt.run(app)
-    
+
+
+@app.route('/connect_port')
+def list_ports():
+	return get_feather_duo_ports()
