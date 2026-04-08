c5ee54ad0981fd78f0d7c50571a7c284d77d5318
Zyun Lam <zyunlam@gmail.com>
2026-04-07 20:51:42 -0500

    Run stuff

--- gss-combiner/server.py
+++ gss-combiner/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from serial.tools.list_ports import comports
 from flask import request
 from flask import jsonify
+import threading
 import standalone
 
 def get_feather_duo_ports():
@@ -20,13 +21,27 @@ app = Flask(__name__)
 
 thread_pool = {}
 
+def feather_connection(port, ip, stage, should_log):
+    try:
+        t = standalone.TelemetryStandalone(port, ip, stage, should_log)
+        t.run()
+    except:
+        return False
+    return True
+
+
+def start_thread(port, ip, stage, should_log):
+    ...
+
 CORS(app)
 
-@app.route('/list_ports')
+@app.route('/ports')
 def list_ports():
 	return jsonify({"devices": get_feather_duo_ports()})
 
-
+@app.route('/feathers')
+def list_feathers():
+    ... # List all the data related to the feather
 
 @app.route('/connect_port', methods=["POST"])
 def connect_port():
